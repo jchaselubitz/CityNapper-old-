2526126b1f9d28ff557ec8f18887537d4d93f145
jchaselubitz <jchaselubitz@gmail.com>
2019-04-15 11:17:45 +0100

    push notification sound now works

--- src/services/pushNotification.js
+++ src/services/pushNotification.js
@@ -1,7 +1,5 @@
 
 import { PushNotificationIOS } from 'react-native';
-import fire_truck_air_horn from '../media/fire_truck_air_horn.wav'
-import church_chime from '../media/church_chime.wav'
 
 const requestPermissions = () => {
   PushNotificationIOS.requestPermissions({
@@ -16,7 +14,7 @@ const localNotification = (destName) => {
     alertTitle:"Wake up!!",
     alertBody: `You are about to arrive at ${destName}.`, 
     playSound: true, // (optional) default: true
-    soundName: 'church_chime', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+    soundName: 'church_chime.wav' // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
   }) 
   // cancelAllLocalNotifications()
 }
